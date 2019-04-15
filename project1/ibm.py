2551c19841114cd5b868559b4e580c95b5432857
Fr Meyer <18168604@sun.ac.za>
2019-04-15 16:03:51 +0200

    EM implemented for IBM1, still to be tested.

--- project1/ibm.py
+++ project1/ibm.py
@@ -6,6 +6,7 @@ Python Version: 3.6
 Implementations of IBM models 1 and 2.
 """
 
+from collections import defaultdict
 
 def preprocess(line):
     """
@@ -34,35 +35,92 @@ def get_vocab(file):
                 vocab.add(word)
     return vocab
 
+def get_corpus(e_file, f_file):
+
+    fe = open(e_file)
+    ff = open(f_file)
+    for e_sent, f_sent in zip(fe, ff):
+        yield e_sent.split(), f_sent.split()
+
+
 class IBM(object):
 
     def __init__(self, model=1):
         self.model = model
         self.t = None
+        self.e_vocab = None
+        self.f_vocab = None
 
-    def train(self, e_file="training/hansards.36.2.e", f_file="training/hansards.36.2.f"):
+    def train(self, e_file="training/hansards.36.2.e", f_file="training/hansards.36.2.f", iters=10):
 
-        e_vocab = get_vocab(e_file)
-        f_vocab = get_vocab(f_file)
+        self.e_vocab = get_vocab(e_file)
+        self.f_vocab = get_vocab(f_file)
 
-        # Model t(f|e) as t[e][f]
-        self.t = self.initialise_params(e_vocab, f_vocab, model=1)
+        self.initialise_params()
+        self.EM(e_file, f_file, iters)
 
 
-    def initialise_params(self, e_vocab, f_vocab, model=1):
+    def EM(self, e_file, f_file, iters):
 
-        if model == 1:
+        # Train parameters with EM algorithm
+        for i in range(iters):
 
-            t = {e_word: {f_word: 1.0/len(f_vocab)} for f_word in f_vocab for e_word in e_vocab}
 
-        return t
+            if self.model == 1:
+
+                # All counts to zero for the new iteration
+                pair_counts = defaultdict(float)
+                word_counts = defaultdict(float)
+
+                # Expectation step
+                for e_sent, f_sent in get_corpus(e_file, f_file):
+
+                    normalise = {}
+                    for f_word in f_sent:
+
+                        # Sum translation probabilities of f words over all e words
+                        normalise[f_word] = 0.0
+                        for e_word in e_sent:
+                            normalise[f_word] += self.t[e_word][f_word]
+
+                        # Update counts
+                        for e_word in e_sent:
+                            delta = self.t[e_word][f_word] / normalise[f_word]
+                            pair_counts[(e_word, f_word)] += delta
+                            word_counts[e_word] += delta
+
+                # Maximisation step
+                for e_word in self.e_vocab:
+                    for f_word in self.f_vocab:
+                        self.t[e_word][f_word] = pair_counts[(e_word, f_word)] / word_counts[e_word]
+
+
+            elif self.model == 2:
+                # EM for IBM2
+                pass
+
+
+    def initialise_params(self):
+
+        if self.model == 1:
+
+            # Store t(f|e) as t[e][f]
+            initial_value = 1.0/len(self.f_vocab)
+            self.t = {e_word: {f_word: initial_value for f_word in self.f_vocab} for e_word in self.e_vocab}
+
+        elif self.model == 2:
+
+            # Initialise IBM2 parameters (some of which will be the same)
+            pass
+
 
 def main():
     model = IBM()
-    model.train()
+    model.train(e_file="mock/e", f_file="mock/f")
+    #model.train()
 
-    print(model.t['primordial']['arrêtait'])
-    print(model)
+    #print(model.t['b']['x'])
+    print(model.t)
 
 if __name__ == "__main__":
     main()
